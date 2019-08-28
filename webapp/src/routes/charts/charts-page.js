30ad386cab111155bb4875e0911b7c2ab3fbc36b
Mitchell Tenney <mitchell.tenney@vivint.com>
2019-08-27 21:20:37 -0600

    Filters working

--- webapp/src/routes/charts/charts-page.js
+++ webapp/src/routes/charts/charts-page.js
@@ -1,21 +1,97 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect } from 'react'
 import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
 
 import PolarChart from './polar-chart'
-import ChartsToolbar from './charts-toolbar'
 
-import Paper from '@material-ui/core/Paper'
+import {
+  Toolbar,
+  TextField,
+  MenuItem,
+  Paper
+} from '@material-ui/core'
 
 export function Charts () {
   const [transactions, setTransactions] = React.useState([])
   const [merchants, setMerchants] = React.useState([])
   const [users, setUsers] = React.useState([])
 
+  const [filters, setFilters] = React.useState({
+    date: 'All',
+    merchant: 'All',
+    user: 'All'
+  })
+  const [filteredTransactions, setFilteredTransactions] = React.useState([])
+
+  const handleChange = name => event => {
+    setFilters({ ...filters, [name]: event.target.value })
+  }
+
+  useEffect(() => {
+    const filtered = handleFiltersChanged(filters)
+    setFilteredTransactions(filtered)
+    console.log(filters)
+  }, [filters])
+
+  const getAvailableDates = () => {
+    var result = ['All']
+    transactions.forEach((transaction, index) => {
+      if (result.indexOf(transaction.date) === -1) {
+        result.push(transaction.date)
+      }
+    })
+    return result
+  }
+
+  const getAvailableMerchants = () => {
+    var result = [{ id: 'all', name: 'All' }]
+    transactions.forEach((transaction, index) => {
+      if (result.indexOf(transaction.merchant_id) === -1) {
+        const merchant = merchants.find(merchant => merchant.id === transaction.merchant_id)
+        if (merchant) {
+          result.push(merchant)
+        }
+      }
+    })
+    return result
+  }
+
+  const getAvailableUsers = () => {
+    var result = [{ id: 'all', name: 'All' }]
+    transactions.forEach((transaction, index) => {
+      if (result.indexOf(transaction.user_id) === -1) {
+        const user = users.find(user => user.id === transaction.user_id)
+        if (user) {
+          result.push(user)
+        }
+      }
+    })
+    return result
+  }
+
   const handleDataLoaded = (data) => {
     setTransactions(data.transactions)
     setMerchants(data.merchants)
     setUsers(data.users)
+  }
+
+  const handleFiltersChanged = (filters) => {
+    var result = []
+    const allDates = filters.date === 'All'
+    const allMerchants = filters.merchant === 'All'
+    const allUsers = filters.user === 'All'
+
+    transactions.forEach((transaction, index) => {
+      const validDate = allDates || transaction.date === filters.date
+      const validMerchant = allMerchants || transaction.merchant_id === merchants.find(merchant => merchant.name === filters.merchant).id
+      const validUser = allUsers || transaction.user_id === users.find(user => user.name === filters.user).id
+
+      if (validDate && validMerchant && validUser) {
+        result.push(transaction)
+      }
+    })
+
+    return result
   }
 
   return (
@@ -52,12 +128,65 @@ export function Charts () {
           return (
             <Fragment>
               <Paper>
-                <ChartsToolbar
-                  merchants={merchants}
-                  transactions={transactions}
-                  users={users}
-                />
-                <PolarChart />
+                <Toolbar>
+                  <TextField
+                    css={{
+                      width: 250
+                    }}
+                    id='outlined-select-date'
+                    label='Date'
+                    margin='normal'
+                    onChange={handleChange('date')}
+                    select
+                    value={filters.date}
+                    variant='outlined'
+                  >
+                    {getAvailableDates().map(date => (
+                      <MenuItem key={date} value={date}>
+                        {date}
+                      </MenuItem>
+                    ))}
+                  </TextField>
+                  <TextField
+                    css={{
+                      width: 250,
+                      marginLeft: 16
+                    }}
+                    id='outlined-select-merchant'
+                    label='Merchant'
+                    margin='normal'
+                    onChange={handleChange('merchant')}
+                    select
+                    value={filters.merchant}
+                    variant='outlined'
+                  >
+                    {getAvailableMerchants().map(merchant => (
+                      <MenuItem key={merchant.id} value={merchant.name}>
+                        {merchant.name}
+                      </MenuItem>
+                    ))}
+                  </TextField>
+                  <TextField
+                    css={{
+                      width: 250,
+                      marginLeft: 16
+                    }}
+                    id='outlined-select-user'
+                    label='User'
+                    margin='normal'
+                    onChange={handleChange('user')}
+                    select
+                    value={filters.user}
+                    variant='outlined'
+                  >
+                    {getAvailableUsers().map(user => (
+                      <MenuItem key={user.id} value={user.name}>
+                        {user.name}
+                      </MenuItem>
+                    ))}
+                  </TextField>
+                </Toolbar>
+                <PolarChart transactions={filteredTransactions} />
               </Paper>
             </Fragment>
           )
--- webapp/src/routes/charts/charts-toolbar.js
+++ webapp/src/routes/charts/charts-toolbar.js
@@ -1,25 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import PropTypes from 'prop-types'
-import {
-  Toolbar,
-  TextField,
-  MenuItem
-} from '@material-ui/core'
+
 
 export default function ChartsToolbar (props) {
-  const { users, merchants, transactions } = props
-
-  const [values, setValues] = React.useState({
-    date: ''
+  const { users, merchants, transactions, handleFiltersChangedCallback } = props
+  const [filters, setFilters] = React.useState({
+    date: 'All',
+    merchant: 'All',
+    user: 'All'
   })
 
   const handleChange = name => event => {
+    // This is an async call, so it isn't reliable. However, using useEffect causes 
+    // infinite render loop. Rock, meet hard place.
     setValues({ ...values, [name]: event.target.value })
   }
 
+  useEffect(() => {
+    // Using this causes an infinite render loop and crashes the app...
+    // handleFiltersChangedCallback(values)
+  })
+
   const getAvailableDates = () => {
-    var result = []
+    var result = ['All']
     transactions.forEach((transaction, index) => {
       if (result.indexOf(transaction.date) === -1) {
         result.push(transaction.date)
@@ -28,11 +32,27 @@ export default function ChartsToolbar (props) {
     return result
   }
 
-  const getTransactionsForDate = (date) => {
-    var result = []
+  const getAvailableMerchants = () => {
+    var result = [{ id: 'all', name: 'All' }]
     transactions.forEach((transaction, index) => {
-      if (transaction.date === date) {
-        result.push(transaction)
+      if (result.indexOf(transaction.merchant_id) === -1) {
+        const merchant = merchants.find(merchant => merchant.id === transaction.merchant_id)
+        if (merchant) {
+          result.push(merchant)
+        }
+      }
+    })
+    return result
+  }
+
+  const getAvailableUsers = () => {
+    var result = [{ id: 'all', name: 'All' }]
+    transactions.forEach((transaction, index) => {
+      if (result.indexOf(transaction.user_id) === -1) {
+        const user = users.find(user => user.id === transaction.user_id)
+        if (user) {
+          result.push(user)
+        }
       }
     })
     return result
@@ -58,11 +78,50 @@ export default function ChartsToolbar (props) {
           </MenuItem>
         ))}
       </TextField>
+      <TextField
+        css={{
+          width: 250,
+          marginLeft: 16
+        }}
+        id='outlined-select-merchant'
+        label='Merchant'
+        margin='normal'
+        onChange={handleChange('merchant')}
+        select
+        value={values.merchant}
+        variant='outlined'
+      >
+        {getAvailableMerchants().map(merchant => (
+          <MenuItem key={merchant.id} value={merchant.name}>
+            {merchant.name}
+          </MenuItem>
+        ))}
+      </TextField>
+      <TextField
+        css={{
+          width: 250,
+          marginLeft: 16
+        }}
+        id='outlined-select-user'
+        label='User'
+        margin='normal'
+        onChange={handleChange('user')}
+        select
+        value={values.user}
+        variant='outlined'
+      >
+        {getAvailableUsers().map(user => (
+          <MenuItem key={user.id} value={user.name}>
+            {user.name}
+          </MenuItem>
+        ))}
+      </TextField>
     </Toolbar>
   )
 }
 
 ChartsToolbar.propTypes = {
+  handleFiltersChangedCallback: PropTypes.func.isRequired,
   users: PropTypes.arrayOf(PropTypes.shape({
     id: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired
